5e240039f0346a73ceead87e69a77650f51a4a39
Konstantin <Konstantin.Borisov@Outlook.com>
2024-10-08 13:34:41 +0300

    Замена кода в файле main.py ветки main

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-a = 3 # this is a comment
-b = 5
-if a > b:
-    print("a is greater than b")
-elif a < b:
-    print("a is less than b")
-else:
-    print("a is equal to b")
+print("Замена кода в файле main.py")
